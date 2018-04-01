209106af414181f2bbef3291b2edb39ccf09441e
erikmukk <34135105+erikmukk@users.noreply.github.com>
2018-04-01 16:23:04 +0300

    Tegin mõned klassid
    Potiknoi - seal hakkab mäng toimuma loodetavasti.
    Mängija - seal on mängija atribuudid.
    KäesKaardid - seal hoiustatakse ja tegeletakse mängija käega.

---
+++ src/Mängija.java
@@ -0,0 +1,2 @@
+public class Mängija {
+}
--- src/Testklass.java
+++ src/Testklass.java
@@ -1,7 +1,4 @@
-
 public class Testklass {
     public static void main(String[] args) {
-        Kaardipakk pakk = new Kaardipakk("väike");
-        System.out.println(pakk.getPoti());
     }
 }

ad032a1ab5170cdc043bed23f036846eabbfa269
Jaakob <37977470+JaakobJ@users.noreply.github.com>
2018-04-04 00:33:30 +0300

    Lisasin TestTrumpi meetodi tapmiseks ja käimiseks
    Kommentaarid on olemas ja klassis Testklass näidatakse, kuidas neid kasutatakse.

--- src/TestTrump.java
+++ src/TestTrump.java
@@ -69,12 +69,48 @@ public class TestTrump {
         System.out.println("Suur tugevuslist on: " + koopia);
     }
 
-
-    // Hakkan siia tapmismeetodit tegema
-/*    public void tapa(Kaart tapetav, List<Kaart> käesOlevadKaardid) {
+    // Meetod, millega arvuti kaarte tappa saab. Sisestada tuleb tapetav kaart ja käes olevad kaardid.
+    // Tagastatakse üheelemendiline list, mille sees on kaart, millega saab tappa või tühi list kui tappa ei saa.
+    // Klass kasutab liste risti, ruutu, poti ärtu
+    public List<Kaart> Tapa(Kaart tapetav, List<Kaart> käesOlevadKaardid) {
         List<Kaart> vastus = new ArrayList<>();
-        List<Kaart> kasutatav = new ArrayList<>();
+        List<Kaart> kasutatavPakk = new ArrayList<>();
+        char mast = tapetav.getMast();
+        if (mast == '♣') {
+            kasutatavPakk.addAll(risti);
+        }
+        else if (mast == '♦') {
+            kasutatavPakk.addAll(ruutu);
+        }
+        else if (mast == '♠') {
+            kasutatavPakk.addAll(poti);
+        }
+        else if (mast == '♥') {
+            kasutatavPakk.addAll(ärtu);
+        }
 
-    }*/
+        for(int i = kasutatavPakk.size() - 1; i >= 0; i--) {
+            if (i < kasutatavPakk.indexOf(tapetav)) {
+                if (käesOlevadKaardid.contains(kasutatavPakk.get(i))) {
+                    vastus.add(kasutatavPakk.get(i));
+                    return vastus;
+                }
+            }
+        }
+        return vastus;
+    }
+
+    // Meetod, millega arvuti käib alati kõige nõrgema kaardi. Sisestatakse arvuti käes olevate kaartide list.
+    // Tagastatakse kaart, mille arvuti käib
+    // Meetod kasutab tugevuslisti nimega koopia
+    public Kaart Käi(List<Kaart> käesOlevadKaardid) {
+        for(int i = koopia.size() - 1; i >= 0; i--) {
+            if (käesOlevadKaardid.contains(koopia.get(i))) {
+                return koopia.get(i);
+            }
+        }
+        return käesOlevadKaardid.get(0); // Seda return'i kunagi ei kasutata, aga selle peab siia panema, muidu pole võimalik meetodit kirja panna
+    }
+
 
 }
--- src/Testklass.java
+++ src/Testklass.java
@@ -1,3 +1,5 @@
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 public class Testklass {
@@ -12,7 +14,30 @@ public class Testklass {
         System.out.println("Trump on: " + trump);
 
         testTrump.TrumbiAktiveerimine(trump);
-        testTrump.prindiMastid();
+        //testTrump.prindiMastid();
 
+
+        //Testin meetodi Tapa
+        List<Kaart> käesOlevadKaardid = Arrays.asList(pakk.get(12), pakk.get(16), pakk.get(4), pakk.get(29), pakk.get(39));
+        System.out.println("Käes olevad kaardid on: " + käesOlevadKaardid);
+        Kaart tapetav1 = pakk.get(20);
+        System.out.println("Tapetav on: " + tapetav1);
+        List<Kaart> tapanSellega1 = testTrump.Tapa(tapetav1, käesOlevadKaardid);
+        System.out.println("Tapan selle kaardiga: " + tapanSellega1);
+
+        Kaart tapetav2 = pakk.get(1);
+        System.out.println("Tapetav on: " + tapetav2);
+        List<Kaart> tapanSellega2 = testTrump.Tapa(tapetav2, käesOlevadKaardid);
+        System.out.println("Tapan selle kaardiga: " + tapanSellega2);
+
+        Kaart tapetav3 = pakk.get(19);
+        System.out.println("Tapetav on: " + tapetav3);
+        List<Kaart> tapanSellega3 = testTrump.Tapa(tapetav3, käesOlevadKaardid);
+        System.out.println("Tapan selle kaardiga: " + tapanSellega3); // Kuna tappa polnud võimalik, tagastati tühi list
+
+
+        // Testin meetodi Käi
+        Kaart käidavKaart = testTrump.Käi(käesOlevadKaardid);
+        System.out.println("Käidav kaart on: " + käidavKaart); // Käidav kaart on õige, sest 5♥ on trump
     }
 }

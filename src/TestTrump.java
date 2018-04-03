5710eae1fb8388477eb22fe920f91cfa4c6d4bbf
Jaakob <37977470+JaakobJ@users.noreply.github.com>
2018-04-03 22:22:20 +0300

    meetod TrumbiAktiveerimine teeb ka ühe suure tugevuslisti

--- src/TestTrump.java
+++ src/TestTrump.java
@@ -10,22 +10,33 @@ public class TestTrump {
     List<Kaart> poti = uusPakk.getPoti();
     List<Kaart> ärtu = uusPakk.getÄrtu();
 
+    List<Kaart> koopia = new ArrayList<>(pakk); // loob koopia listist pakk nimega koopia
+
     // Anna parameetriks trump-kaart ja meetod lisab trumbi masti kaardid ülejäänud mastide listidesse
-    // NB! pakk, risti, ruutu, poti ärtu listid peavad varasemalt olemas olema!
+    // NB! pakk, koopia, risti, ruutu, poti ärtu listid peavad varasemalt olemas olema!
+    // NB! meetod SuurTugevuslist on vaja, sest TrumbiAktiveerimine kasutab seda!
     public void TrumbiAktiveerimine(Kaart trump) {
         List<Kaart> lisatav = new ArrayList<>();
         char mast = trump.getMast();
         if (mast == '♣') {
             lisatav.addAll(risti);
+            SuurTugevuslist('♣');
+            koopia.addAll(0, risti);
         }
         else if (mast == '♦') {
             lisatav.addAll(ruutu);
+            SuurTugevuslist('♦');
+            koopia.addAll(0, ruutu);
         }
         else if (mast == '♠') {
             lisatav.addAll(poti);
+            SuurTugevuslist('♠');
+            koopia.addAll(0, poti);
         }
         else if (mast == '♥') {
             lisatav.addAll(ärtu);
+            SuurTugevuslist('♥');
+            koopia.addAll(0, ärtu);
         }
         if (!(mast == '♣')) {
             risti.addAll(0, lisatav);
@@ -41,11 +52,29 @@ public class TestTrump {
         }
     }
 
+    private void SuurTugevuslist(char mast) {
+        for (int i = 0; i < koopia.size(); i++) {
+            if (koopia.get(i).getMast() == mast) {
+                koopia.remove(i);
+            }
+        }
+    }
+
     public void prindiMastid() {
         System.out.println("Risti pakk: " + risti);
         System.out.println("Ruutu pakk: " + ruutu);
         System.out.println("Poti pakk: " + poti);
         System.out.println("Ärtu pakk: " + ärtu);
+
+        System.out.println("Suur tugevuslist on: " + koopia);
     }
+
+
+    // Hakkan siia tapmismeetodit tegema
+/*    public void tapa(Kaart tapetav, List<Kaart> käesOlevadKaardid) {
+        List<Kaart> vastus = new ArrayList<>();
+        List<Kaart> kasutatav = new ArrayList<>();
+
+    }*/
 
 }

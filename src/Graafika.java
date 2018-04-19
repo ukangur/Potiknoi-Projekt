51fe3811f5e48f0513709fff6999efe1644ee58c
ukangur <34269763+ukangur@users.noreply.github.com>
2018-04-19 10:31:31 +0300

    Graafika algus

---
+++ src/Graafika.java
@@ -0,0 +1,157 @@
+import java.awt.*;
+import java.util.concurrent.Executors;
+import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.TimeUnit;
+
+import javafx.animation.FadeTransition;
+import javafx.animation.ParallelTransition;
+import javafx.animation.TranslateTransition;
+import javafx.application.Application;
+import javafx.application.Platform;
+import javafx.geometry.Pos;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.effect.GaussianBlur;
+import javafx.scene.input.KeyCode;
+import javafx.scene.layout.*;
+import javafx.scene.paint.Color;
+import javafx.scene.shape.Circle;
+import javafx.scene.shape.Rectangle;
+import javafx.scene.shape.Shape;
+import javafx.scene.text.Font;
+import javafx.scene.text.FontWeight;
+import javafx.scene.text.Text;
+import javafx.stage.Stage;
+import javafx.scene.image.Image;
+import javafx.util.Duration;
+
+
+//menuitemi klassi sain netist
+
+public class Graafika extends Application {
+
+    private static final Font FONT = Font.font("", FontWeight.BOLD, 18);
+
+    private VBox menuBox;
+    private int currentItem = 0;
+
+    private int messages = 0;
+
+    private ScheduledExecutorService bgThread = Executors.newSingleThreadScheduledExecutor();
+
+    private Parent LooSisu() {
+        Pane root = new Pane();
+        root.setPrefSize(900, 600);
+
+        BackgroundImage piltbg = new BackgroundImage(new Image("Taust.png",900,600,false,true),
+                BackgroundRepeat.REPEAT, BackgroundRepeat.NO_REPEAT, BackgroundPosition.DEFAULT,
+                BackgroundSize.DEFAULT);
+
+
+
+ Rectangle bg = new Rectangle(900, 600);
+ bg.setFill(Color.BLUE);
+
+        MenuItem itemExit = new MenuItem("Välju Mängust");
+        itemExit.setOnActivate(() -> System.exit(0));
+
+        menuBox = new VBox(20,
+                new MenuItem("Mängima"),
+                new MenuItem("Seaded"),
+                new MenuItem("Muusika valik"),
+                new MenuItem("Õpetus"),
+                itemExit);
+        menuBox.setAlignment(Pos.TOP_CENTER);
+        menuBox.setTranslateX(360);
+        menuBox.setTranslateY(300);
+
+        getMenuItem(0).setActive(true);
+
+        root.getChildren().addAll(bg, menuBox);
+
+        return root;
+    }
+
+    private MenuItem getMenuItem(int index) {
+        return (MenuItem)menuBox.getChildren().get(index);
+    }
+
+    private static class SisuRaam extends StackPane {
+        public SisuRaam(Node sisu) {
+            setAlignment(Pos.CENTER);
+
+            Rectangle raam = new Rectangle(200, 200);
+            raam.setArcWidth(25);
+            raam.setArcHeight(25);
+            raam.setStroke(Color.WHITESMOKE);
+
+            getChildren().addAll(raam, sisu);
+        }
+    }
+
+    private static class MenuItem extends HBox {
+        private Text text;
+        private Runnable script;
+
+        public MenuItem(String name) {
+            super(15);
+            setAlignment(Pos.CENTER);
+
+            text = new Text(name);
+            text.setFont(FONT);
+            text.setEffect(new GaussianBlur(2));
+
+            getChildren().addAll(text);
+            setActive(false);
+            setOnActivate(() -> System.out.println(name + " aktiveeritud"));
+        }
+
+        public void setActive(boolean b) {
+            text.setFill(b ? Color.WHITE : Color.GREY);
+        }
+
+        public void setOnActivate(Runnable r) {
+            script = r;
+        }
+
+        public void activate() {
+            if (script != null)
+                script.run();
+        }
+    }
+
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        Scene scene = new Scene(LooSisu());
+        scene.setOnKeyPressed(event -> {
+            if (event.getCode() == KeyCode.UP) {
+                if (currentItem > 0) {
+                    getMenuItem(currentItem).setActive(false);
+                    getMenuItem(--currentItem).setActive(true);
+                }
+            }
+
+            if (event.getCode() == KeyCode.DOWN) {
+                if (currentItem < menuBox.getChildren().size() - 1) {
+                    getMenuItem(currentItem).setActive(false);
+                    getMenuItem(++currentItem).setActive(true);
+                }
+            }
+
+            if (event.getCode() == KeyCode.ENTER) {
+                getMenuItem(currentItem).activate();
+            }
+        });
+
+        primaryStage.setScene(scene);
+        primaryStage.setOnCloseRequest(event -> {
+            bgThread.shutdownNow();
+        });
+        primaryStage.show();
+    }
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
